a0ff7f843915a12a4e8d8ba3c3b7dd60c9a39b5c
osoufan_t <osoufan@aidm.ai>
2022-04-20 10:58:53 -0300

    Starting my hello Changzhou C++ project

---
+++ src/helloworld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ChangzhouStFXLec1.cpp
+// Author      : Othman Soufan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Changzhou Students!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
