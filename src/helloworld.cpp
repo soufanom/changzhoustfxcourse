f38ddd94d927ae7dbd0c931a86c6fc8e76afb38b
osoufan_t <osoufan@aidm.ai>
2022-04-27 10:43:53 -0300

    Swap example

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -9,9 +9,23 @@
 #include <iostream>
 using namespace std;
 
+void swap(int& first, int& second){
+	int temp = first;
+	first = second;
+	second = temp;
+}
+
 int main() {
-	double weight = 10.9;
-	cout << "!!!Hello Changzhou Students!!!" << endl; // prints !!!Hello World!!!
-	cout << "my weight is " << weight << endl;
+	int a = 2, b = 3;
+
+	cout << "before calling swap" << endl;
+	cout << a << " " << b << endl;
+
+	swap(a, b);
+
+	cout << "after calling swap" << endl;
+	cout << a << " " << b << endl;
+
+	//what is the output of the program??
 	return 0;
 }
